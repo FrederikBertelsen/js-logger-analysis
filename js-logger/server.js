329af3e414093378e5f3c2f9dc84ea47bf8694b7
Frederik Bertelsen <frbb@itu.dk>
2025-02-18 14:32:44 +0100

    added index.html

--- js-logger/server.js
+++ js-logger/server.js
@@ -1,12 +1,24 @@
 const http = require('http');
+const fs = require('fs');
+
 
 const server = http.createServer((req, res) => {
     if (req.method === 'GET' && req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify({ message: 'Hello world' }));
+    }
+    else if (req.method === 'GET' && req.url === '/index.html') {
+        fs.readFile('index.html', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                return res.end('Error loading index.html');
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
     } else {
-        res.writeHead(404);
-        res.end();
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not found');
     }
 });
 
