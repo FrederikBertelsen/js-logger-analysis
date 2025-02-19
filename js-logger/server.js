eaaeda1ec04d5d6156ab10a97498f6acda4d3e98
Frederik Bertelsen <frbb@itu.dk>
2025-02-19 09:41:22 +0100

    fix wrong port

--- js-logger/server.js
+++ js-logger/server.js
@@ -22,4 +22,4 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(3000, () => console.log('Server running on port 3000'));
+server.listen(4000, () => console.log('Server running on port 3000'));
