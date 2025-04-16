7d80df8fcc0589347939c6f8c10df12185f2012e
Frederik Bertelsen <frbb@itu.dk>
2025-04-16 12:56:20 +0200

    add download string feature

---
+++ js-logger/public/js/download-string.js
@@ -0,0 +1,18 @@
+(function () {
+    function downloadTextFile(text, filename = 'download.txt') {
+        const blob = new Blob([text], { type: 'text/plain' });
+        const url = URL.createObjectURL(blob);
+
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = filename;
+
+        document.body.appendChild(a);
+        a.click();
+
+        setTimeout(() => {
+            document.body.removeChild(a);
+            URL.revokeObjectURL(url);
+        }, 0);
+    }
+})();
