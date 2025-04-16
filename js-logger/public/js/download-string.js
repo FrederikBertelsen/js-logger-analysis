f5d5264a2cdc2142794260fabd3387c879b73a05
Frederik Bertelsen <frbb@itu.dk>
2025-04-16 13:11:38 +0200

    fix

--- js-logger/public/js/download-string.js
+++ js-logger/public/js/download-string.js
@@ -1,18 +1,16 @@
-(function () {
-    function downloadTextFile(text, filename = 'download.txt') {
-        const blob = new Blob([text], { type: 'text/plain' });
-        const url = URL.createObjectURL(blob);
+function downloadTextFile(text, filename = 'download.txt') {
+    const blob = new Blob([text], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
 
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = filename;
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
 
-        document.body.appendChild(a);
-        a.click();
+    document.body.appendChild(a);
+    a.click();
 
-        setTimeout(() => {
-            document.body.removeChild(a);
-            URL.revokeObjectURL(url);
-        }, 0);
-    }
-})();
+    setTimeout(() => {
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+    }, 0);
+}
